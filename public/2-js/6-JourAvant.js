a80d60986c579760e905878b0dea150ad25580fa
Pampa Senior <pampa.senior.code@gmail.com>
2024-05-03 10:47:48 +0200

    - Mise en place de la traduction des messages de résultats

--- public/2-js/6-JourAvant.js
+++ public/2-js/6-JourAvant.js
@@ -2,79 +2,21 @@ function ClicJour()
 {
     const Couleur = 'text-danger';
     const Widget = $(event.currentTarget);
-    const Style = Widget.data('style');
     const Bordure = Widget.data('bordure');
-    const Zoom = Widget.data('zoom');
     const Taille = Widget.data('taille');
     const URL = Widget.data('url');
 
     let Debut = function () {
         Widget.prop('disabled',true);
         Widget.removeClass('AnimationOuvrable');
-        if (Style == 2) {
+        if (Bordure) { //0, false, undefined, null et "" sont considérés comme faux
             Widget.addClass('bloc ' + Bordure + ' ' + Taille);
         }
         Widget.html('<i class="fa-solid fa-spinner fa-spin ' + Couleur + ' cherche ' + Taille + '"></i>');
     }
 
     let Succes = function (Reponse) {
-        if (Reponse.fr || Reponse.en) { //0, false, undefined, null et "" sont considérés comme faux
-            Echec(Reponse.en + " " + Reponse.fr); //La vérification de l'application a échouée
-            return;
-        }
-
-        let Titre = '';
-        let Texte = '';
-
-        if (Taille == 'sm') {
-            Titre = Reponse.Gagnant + "\n" + Reponse.Cadeau;
-            Texte = Reponse.Gagnant + '<br/>' + Reponse.Cadeau;
-        } else if (Taille == 'md') {
-            Titre = 'Gagnant : ' + Reponse.Gagnant + "\nCadeau : " + Reponse.Cadeau;
-            Texte = 'Gagnant :<br/>' + Reponse.Gagnant + '<br/>Cadeau :<br/>' + Reponse.Cadeau;
-        } else if (Taille == 'lg') {
-            Titre = 'Le gagnant : ' + Reponse.Gagnant + "\nLe cadeau : " + Reponse.Cadeau;
-            Texte = 'Le gagnant :<br/>' + Reponse.Gagnant + '<br/>Le cadeau :<br/>' + Reponse.Cadeau;
-        } else {
-            Titre = 'Le gagnant est : ' + Reponse.Gagnant + "\nLe cadeau est : " + Reponse.Cadeau;
-            Texte = 'Le gagnant est :<br/>' + Reponse.Gagnant + '<br/>Le cadeau est :<br/>' + Reponse.Cadeau;
-        }
-
-        if (Zoom == 1) {
-            position = 'position-absolute top-0 start-0';
-        } else if (Zoom == 2) {
-            position = 'position-absolute top-0 start-50';
-        } else if (Zoom == 3) {
-            position = 'position-absolute top-0 start-100';
-        } else if (Zoom == 4) {
-            position = 'position-absolute top-50 start-0';
-        } else if (Zoom == 5) {
-            position = 'position-absolute top-50 start-100';
-        } else if (Zoom == 6) {
-            position = 'position-absolute top-100 start-0';
-        } else if (Zoom == 7) {
-            position = 'position-absolute top-100 start-50';
-        } else if (Zoom == 8) {
-            position = 'position-absolute top-100 start-100';
-        }
-
-        Widget.attr('title', Titre);
-        Widget.addClass('position-relative');
-        Widget.html('<span class="d-block text-truncate ' + Couleur + '">' + Texte + '</span>');
-
-        if (Reponse.Illustration) { //0, false, undefined, null et "" sont considérés comme faux
-            Widget.attr('style', 'background-image:url(' + Reponse.Illustration + ');background-size:cover;');
-        }
-
-        if (Zoom != 0) {
-            Widget.append('<div><button type="button" class="py-2 ' + position + ' translate-middle badge bg-danger border-0" onclick="ClicLoupe()"><i class="fa-solid fa-magnifying-glass"></i></button></div>');
-            Widget.find('div button').attr('data-gagnant', Reponse.Gagnant);
-            Widget.find('div button').attr('data-cadeau', Reponse.Cadeau);
-        }
-
-        if (Zoom != 0 && Reponse.Illustration) {
-            Widget.find('div button').attr('data-photo', Reponse.Illustration);
-        }
+        Widget.replaceWith(Reponse);
     }
 
     let Echec = function (Erreur) {
@@ -92,7 +34,7 @@ function ClicJour()
     $.ajax({
         url: URL,
         method: 'GET',
-        dataType: 'json',
+        dataType: 'html',
         beforeSend: Debut,
         success: Succes,
         error: Echec,
@@ -110,7 +52,7 @@ function JourAvant(J,M)
     const JourMax = Math.min(J, Jours.length);
 
     if (Mois == M - 1) { //Si on est le bon mois
-        for (var i = 0; i <= JourMax - 1; i++) { //Attention les indices de Jours vont de 0 à JourMax-1
+        for (var i = 0; i <= JourMax - 1; i++) { //Attention les indices de Jours vont de 0 à JourMax - 1
             $(Jours[i]).on('click',ClicJour); //Mise en place du clic de révélation jusqu'à aujourd'hui
 
             if (i < JourMax - 1 || J > Jours.length) { //Utilisation de la révélation jusqu'à la veille ou sur tous si on dépasse le dernier jour
--- src/Controller/Ajax.php
+++ src/Controller/Ajax.php
@@ -5,23 +5,53 @@ namespace App\Controller;
 use App\Service\Verification;
 use App\Service\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Contracts\Translation\TranslatorInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/Ajax')]
 class Ajax extends AbstractController
 {
     #[Route('/Cadeau/{Id}', name: 'Cadeau', requirements: ['Id' => '\d+'], methods: ['GET'])]
-    public function cadeau(int $Id, TranslatorInterface $Traducteur): JsonResponse
+    public function cadeau(int $Id): Response
     {
         $Verification = new Verification($this->container->get('parameter_bag'));
         if (!$Verification->isValide()) {
-            $Sortie = [
-                'fr' => $Traducteur->trans($Verification->getErreur(), [], 'messages', 'fr'),
-                'en' => $Traducteur->trans($Verification->getErreur(), [], 'messages', 'en')
-            ];
-            return new JsonResponse($Sortie);
+            return $this->render(
+                'message/information.html.twig',
+                [
+                    'Indentation' => '  ',
+                    'Message' => $Verification->getErreur()
+                ]
+            );
+        }
+
+        switch ($this->getParameter('Bordure')) {
+            case 1:
+                $Bordure = 'border border-success bordure-1';
+                break;
+            case 2:
+                $Bordure = 'border border-success bordure-1 bordure-arrondie';
+                break;
+            case 3:
+                $Bordure = 'border border-success bordure-1 rounded-circle';
+                break;
+            default:
+                $Bordure = 'bordure-0';
+        }
+
+        switch ($this->getParameter('Zoom')) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                $Zoom = $this->getParameter('Zoom');
+                break;
+            default:
+                $Zoom = 0;
         }
 
         $Noel = $this->getParameter('Noel') == 1; //TODO : Mettre cela dans Tirage mais après Verification...
@@ -33,6 +63,11 @@ class Ajax extends AbstractController
         $Mois = $Date->format("n");
 
         $Resultats = array_slice($_ENV['Resultats'], 0, 24 + $Noel, true); /*Récupère 24 éléments sauf si noël est activé TODO : Mettre cela dans Tirage mais après Verification...*/ //phpcs:ignore Generic.Files.LineLength
+
+        $Gagnant = $Pot2Miel['Gagnant'];
+        $Cadeau = $Pot2Miel['Cadeau'];
+        $Illustration = '';
+
         if ($Id <= $Jour && $Mois == $_ENV['Mois'] && isset($Resultats[$Id])) {
             //Pour vérifier l'image d'illustration du cadeau
             if (isset($Resultats[$Id]['Illustration'])) {
@@ -47,9 +82,24 @@ class Ajax extends AbstractController
                 }
             }
 
-            return new JsonResponse($Resultats[$Id]);
+            $Gagnant = $Resultats[$Id]['Gagnant'];
+            $Cadeau = $Resultats[$Id]['Cadeau'];
+            $Illustration = $Resultats[$Id]['Illustration'];
         }
 
-        return new JsonResponse($Pot2Miel);
+        return $this->render(
+            'accueil/calendrier/_jour.html.twig',
+            [
+                'Indentation' => '    ',
+                'Jour' => $Id,
+                'Couleur' => $this->getParameter('CouleurTexte'),
+                'Bordure' => $Bordure,
+                'Zoom' => $Zoom,
+                'Taille' => $this->getParameter('Taille'),
+                'Gagnant' => $Gagnant,
+                'Cadeau' => $Cadeau,
+                'Illustration' => $Illustration
+            ]
+        );
     }
 }
